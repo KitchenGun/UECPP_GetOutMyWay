19833ad01b3e8ee51cd093f414c8c5adfe3f6d3a
KitchenGun <kitchengun124@gmail.com>
2022-01-13 20:35:51 +0900

    Tank Asset 추가

---
+++ README.md
@@ -0,0 +1,3 @@
+# UECPP_GetOutMyWay
+
+Developed with Unreal Engine 4
---
+++ Source/UECPP_GetOutMyWay/UECPP_GetOutMyWayGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UECPP_GetOutMyWayGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UECPP_GETOUTMYWAY_API AUECPP_GetOutMyWayGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
