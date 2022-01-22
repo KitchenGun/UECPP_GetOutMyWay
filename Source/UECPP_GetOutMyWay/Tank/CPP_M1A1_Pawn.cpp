1031895337db7cce04c905e2bbb9eb753ff4c43b
KitchenGun <kitchengun124@gmail.com>
2022-01-22 16:17:46 +0900

    2주차-5
    Character를 상속받아서 제작했는데 Pawn을 상속받아서 제작해서 충돌체 문제를 해결해볼려고 노력중

---
+++ Source/UECPP_GetOutMyWay/Tank/CPP_M1A1_Pawn.cpp
@@ -0,0 +1,26 @@
+#include "Tank/CPP_M1A1_Pawn.h"
+
+ACPP_M1A1_Pawn::ACPP_M1A1_Pawn()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ACPP_M1A1_Pawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ACPP_M1A1_Pawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ACPP_M1A1_Pawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
