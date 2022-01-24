a28345465b43e131e5b505082a558fb0948bb3bc
KitchenGun <kitchengun124@gmail.com>
2022-01-24 18:44:32 +0900

    3주차-1
    멘탈을 다시 잡았다.
    
    Character 에서는 capsule을 상속받기때문에 Pawn으로 제작을 하기로 하였다.
    Test Pawn을 만들어서 충돌 처리를 확인해봤다.
    "Squad"라는 게임의 모드 에디터를 통해서 제작 방법에 팁을 얻었다.  그래서 ue4에서 제공해주는 vehicle comp를 공부해봐서 추가적으로 적용을 해야겠다.
    //
    M1A1 Pawn:
    (Basic)
    제작중 vehicle comp 공부 필요

--- Source/UECPP_GetOutMyWay/Tank/CPP_M1A1_Pawn.h
+++ Source/UECPP_GetOutMyWay/Tank/CPP_M1A1_Pawn.h
@@ -8,6 +8,34 @@ UCLASS()
 class UECPP_GETOUTMYWAY_API ACPP_M1A1_Pawn : public APawn
 {
 	GENERATED_BODY()
+protected:
+	//Root
+	UPROPERTY(EditDefaultsOnly)
+	class UBoxComponent* TankRoot;
+
+	//Mesh
+	UPROPERTY(EditDefaultsOnly)
+	class USkeletalMeshComponent* TankMesh;
+
+	//Camera
+	UPROPERTY(EditDefaultsOnly)
+	class UCameraComponent* Camera;
+	UPROPERTY(EditDefaultsOnly)
+	class USpringArmComponent* SpringArm;
+
+	//Collision
+	UPROPERTY(EditDefaultsOnly)
+	class UBoxComponent* FrontUpper;
+	UPROPERTY(EditDefaultsOnly)
+	class UBoxComponent* FrontUnder;
+	UPROPERTY(EditDefaultsOnly)
+	class UBoxComponent* Engine;
+	UPROPERTY(EditDefaultsOnly)
+	class UBoxComponent* Bottom;
+
+	//ActorComp
+	UPROPERTY(EditDefaultsOnly)
+	class UCPP_TrackMovementComponent* TrackMovement;
 
 public:
 	ACPP_M1A1_Pawn();
@@ -19,5 +47,7 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
 
 };
